16bbc1fbf1a6aa2e3dcf0c17ce1dfc9b04791599
VexedUXR <ahmadkhalifa570@gmail.com>
2023-12-27 20:16:02 +0200

    Dont unbuffer stdout

--- main.c
+++ main.c
@@ -318,7 +318,8 @@ MainParseArgDebug(const char *argvalue)
 finish:
 	opts.debug = debug;
 
-	setvbuf(opts.debug_file, NULL, _IONBF, 0);
+	if (opts.debug_file != stdout)
+		setvbuf(opts.debug_file, NULL, _IONBF, 0);
 }
 
 /* Is path relative or does it contain any relative component "." or ".."? */
